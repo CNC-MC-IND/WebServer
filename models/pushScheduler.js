f2c553b391ee26535c464b439d62db5eaea153c8
jumpsnack <kir0302@gmail.com>
2017-09-25 14:35:14 +0900

    [edit] push

--- models/pushScheduler.js
+++ models/pushScheduler.js
@@ -11,7 +11,7 @@ exports.start = function () {
     loopFlag = true
     async.forever(
         function (next) {
-            if(!loopFlag) {
+            if (!loopFlag) {
                 console.log('pushScheduler stopped!')
                 return
             }
@@ -25,31 +25,63 @@ exports.start = function () {
                         throw err;
 
                     var item
-                    for(var i=0; i<rows.length; i++){
+                    var msg = ''
+                    for (var i = 0; i < rows.length; i++) {
                         item = rows[i]
                         compVal |= item.lubricant_machine |
                             item.lubricant_saw |
                             item.pressure_air_main |
                             item.pressure_oil_hydraulic |
-                            item.servo_cut|
-                            item. servo_transfer|
-                            item. spindle|
-                            item. safety_door|
+                            item.servo_cut |
+                            item.servo_transfer |
+                            item.spindle |
+                            item.safety_door |
                             item.depletion
+
+                        if (compVal) {
+                            msg = item.id.toString()
+
+                            if (item.lubricant_machine) {
+                                msg += '장비 윤활유 부족\n'
+                            }
+                            if (item.lubricant_sawr) {
+                                msg += '톱날 윤활유 부족\n'
+                            }
+                            if (item.pressure_air_main) {
+                                msg += '메인 공기압 부족\n'
+                            }
+                            if (item.pressure_oil_hydraulic) {
+                                msg += '유압유 압력 부족\n'
+                            }
+                            if (item.servo_cut) {
+                                msg += '절단 서보 에러\n'
+                            }
+                            if (item.servo_transfer) {
+                                msg += '이송 서보 에러\n'
+                            }
+                            if (item.spindle) {
+                                msg += '스핀들 이상 에러\n'
+                            }
+                            if (item.safety_door) {
+                                msg += '안전문 이상\n'
+                            }
+                            if (item.depletion) {
+                                msg += '소재 부족\n'
+                            }
+                        }
+                        if (compVal == 1)
+                            toolBox.broadcastPush(msg)
                     }
-
-                    if(compVal == 1)
-                        toolBox.broadcastPush('CNC M/C Monitor','장비에 이상이 있습니다!')
-
                     connexion.release();
-                });})
+                });
+            })
 
 
-            setTimeout(function() {
+            setTimeout(function () {
                 next();
             }, delay)
         },
-        function(err) {
+        function (err) {
             console.error(err);
         }
     )
