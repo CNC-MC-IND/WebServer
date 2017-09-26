f6d1d252027efe007c97af93e68d0c9b0846a53b
Gina <jinhwajung93@gmail.com>
2017-09-26 15:38:33 +0900

    [edit] renewed

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var bodyParser = require('body-parser');
 const config = require('./config');
 const configDB = require('./configDB');
 var mysql = require('mysql');
-var toolBox = require('./models/toolBox')
-var pushScheduler = require('./models/pushScheduler')
+var toolBox = require('./models/toolBox');
+var pushScheduler = require('./models/pushScheduler');
 
 
 var index = require('./routes/index');
@@ -16,23 +16,24 @@ var join = require('./routes/join');
 var establish = require('./routes/establish');
 var admin_users = require('./routes/admin/users');
 var admin_approve = require('./routes/admin/approve');
-var admin_instantQuery = require('./routes/admin/instantQuery')
+var admin_instantQuery = require('./routes/admin/instantQuery');
+var admin_signupList = require('./routes/admin/signupList');
 var account_cert = require('./routes/account/cert');
 var account_signup = require('./routes/account/signup');
 var account_validate = require('./routes/account/validate');
 var account_reset = require('./routes/account/reset');
 var video_upload = require('./routes/video/upload');
-var video_list = require('./routes/video/list')
-var video_remove = require('./routes/video/remove')
+var video_list = require('./routes/video/list');
+var video_remove = require('./routes/video/remove');
 var video_streaming = require('./routes/video/streaming');
 var video_downstream = require('./routes/video/downstream');
-var video_getInterval = require('./routes/video/getInterval')
-var video_setInterval = require('./routes/video/setInterval')
-var data_renewedData = require('./routes/data/renewedData')
-var data_insert = require('./routes/data/insert')
-var fcm_compare = require('./routes/FCM/compare')
-var fcm_register = require('./routes/FCM/register')
-var fcm_unregister = require('./routes/FCM/unregister')
+var video_getInterval = require('./routes/video/getInterval');
+var video_setInterval = require('./routes/video/setInterval');
+var data_renewedData = require('./routes/data/renewedData');
+var data_insert = require('./routes/data/insert');
+var fcm_compare = require('./routes/FCM/compare');
+var fcm_register = require('./routes/FCM/register');
+var fcm_unregister = require('./routes/FCM/unregister');
 
 var app = express();
 
@@ -72,8 +73,9 @@ app.use('/', index);
 app.use('/join', join);
 app.use('/establish', establish);
 app.use('/admin/users', admin_users);
-app.use('/admin/instant_query', admin_instantQuery)
+app.use('/admin/instant_query', admin_instantQuery);
 app.use('/admin/approve', admin_approve);
+app.use('/admin/signupList', admin_signupList);
 app.use('/account/cert', account_cert);
 app.use('/account/signup', account_signup);
 app.use('/account/validate', account_validate);
@@ -83,15 +85,15 @@ app.use('/video/list', video_list);
 app.use('/video/remove', video_remove);
 app.use('/video/streaming/*', video_streaming);
 app.use('/video/downstream/*', video_downstream);
-app.use('/video/get_interval', video_getInterval)
-app.use('/video/set_interval', video_setInterval)
-app.use('/data/renewed_data', data_renewedData)
-app.use('/data/insert', data_insert)
-app.use('/fcm/compare', fcm_compare)
-app.use('/fcm/register', fcm_register)
-app.use('/fcm/unregister', fcm_unregister)
+app.use('/video/get_interval', video_getInterval);
+app.use('/video/set_interval', video_setInterval);
+app.use('/data/renewed_data', data_renewedData);
+app.use('/data/insert', data_insert);
+app.use('/fcm/compare', fcm_compare);
+app.use('/fcm/register', fcm_register);
+app.use('/fcm/unregister', fcm_unregister);
 
-pushScheduler.start()
+pushScheduler.start();
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
