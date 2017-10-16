0a408ecb728863abeeb2e7bcefb8b01f3d56d123
JIHYEPYO <pyojihye95@gmail.com>
2017-10-16 13:20:36 +0900

    [edit] detail info

--- routes/main.js
+++ routes/main.js
@@ -9,5 +9,10 @@ router.get('/', function(req, res, next) {
     res.render('main')
 
 });
+router.get('/:id', function(req, res, next) {
+    var id = req.params.id;
+    console.log(id)
+    res.render('detail',{deviceid :id})
 
+});
 module.exports = router;
