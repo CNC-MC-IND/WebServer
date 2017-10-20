df945cf60116664baca1129033ac638c872b24a1
JIHYEPYO <pyojihye95@gmail.com>
2017-10-20 15:13:26 +0900

    [edit] noti

--- routes/admin/approve.js
+++ routes/admin/approve.js
@@ -9,6 +9,29 @@ const configDB = require('../../configDB');
 var pool = mysql.createPool(configDB);
 var toolBox = require('../../models/toolBox');
 
+/* GET approve list*/
+router.get('/', function(req, res, next) {
+    pool.getConnection(function (err, connexion) {
+        if (err)
+            throw err;
+
+        var query = 'select * from users where token =\'-\'';
+        connexion.query(query, function (err, rows) {
+            if (err)
+                throw err;
+
+            console.log(rows);
+            res.render('approve',{rows:rows});
+            /*res.json({
+                type : true,
+                data : JSON.stringify(rows)
+            });
+            */
+            connexion.release();
+        });
+    })
+});
+
 /* PUT approve. */
 router.put('/',function(req, res, next) {
     pool.getConnection(function (err, connexion) {
